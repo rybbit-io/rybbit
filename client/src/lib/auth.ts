53b525ec0c3f54f77e0a48cfd59e8428830928c1
Torben Raab <torben.raab@lila-it.de>
2025-05-10 13:43:14 +0200

    feature: add OIDC as authentication method

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -2,12 +2,18 @@ import {
   adminClient,
   organizationClient,
   usernameClient,
+  genericOAuthClient,
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  plugins: [usernameClient(), adminClient(), organizationClient()],
+  plugins: [
+    usernameClient(),
+    adminClient(),
+    organizationClient(),
+    genericOAuthClient(),
+  ],
   fetchOptions: {
     credentials: "include",
   },
