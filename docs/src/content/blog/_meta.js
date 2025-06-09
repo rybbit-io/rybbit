176b467005f9bdcc980e2141f27a602d585278c5
JMeng1 <mengjoey@msu.edu>
2025-06-08 20:04:42 -0400

    trying to fix content

--- docs/src/content/blog/_meta.js
+++ docs/src/content/blog/_meta.js
@@ -2,5 +2,5 @@ export default {
   "5k-stars":
     "How my open source SaaS got 5,000 Github stars in 9 days with zero marketing effort",
   "website-traffic-drop":
-   " Why did my website traffic drop?"
+   "Why did my website traffic drop?"
 };
