6f8d7d3532149a4127d50e3be6088577b5fd18c7
Andrii Zontov <loginmanx@gmail.com>
2025-09-04 00:26:24 +0200

    feat: Implement dnd sortable blocks logic to improve UX

--- client/src/app/[site]/main/page.tsx
+++ client/src/app/[site]/main/page.tsx
@@ -1,4 +1,17 @@
 "use client";
+
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import React, { useCallback, useEffect, useState } from "react";
+import { SortableItem } from "../../../components/SortableItem";
 import { useSetPageTitle } from "../../../hooks/useSetPageTitle";
 import { useStore } from "../../../lib/store";
 import { SubHeader } from "../components/SubHeader/SubHeader";
@@ -9,23 +22,8 @@ import { Events } from "./components/sections/Events";
 import { Pages } from "./components/sections/Pages";
 import { Referrers } from "./components/sections/Referrers";
 import { Weekdays } from "./components/sections/Weekdays";
-import { DndContext, useDraggable } from "@dnd-kit/core";
 
-function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
-    id: 'draggable',
-  });
-  const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-  } : undefined;
-
-  
-  return (
-    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      {props.children}
-    </button>
-  );
-}
+const blocksMap: { [key: string]: React.FC } = { Devices, Pages, Referrers, Countries, Events, Weekdays };
 
 export default function MainPage() {
   const { site } = useStore();
@@ -34,28 +32,78 @@ export default function MainPage() {
     return null;
   }
 
-  return <MainPageContent />;
+  return <MainPageContent site={site} />;
 }
 
-function MainPageContent() {
+function MainPageContent({ site }: { site: string }) {
   useSetPageTitle("Rybbit · Main");
+
+  const localStorageName = `dashboard-blocks-order-${site}`;
+
+  const blocksPositionOrder = ["Devices", "Pages", "Referrers", "Countries", "Events", "Weekdays"];
+
+  const [blocksOrder, setBlocksOrder] = useState<string[]>(blocksPositionOrder);
+
+  useEffect(() => {
+    const savedOrder = localStorage.getItem(localStorageName);
+
+    if (!savedOrder) return;
+
+    try {
+      const parsedOrder = JSON.parse(savedOrder);
+
+      if (
+        Array.isArray(parsedOrder) &&
+        parsedOrder.every((item) => blocksPositionOrder.includes(item)) &&
+        parsedOrder.length === blocksPositionOrder.length
+      ) {
+        setBlocksOrder(parsedOrder);
+      } else {
+        setBlocksOrder(blocksPositionOrder);
+      }
+    } catch (error) {
+      console.error("Failed to parse saved layout order:", error);
+
+      setBlocksOrder(blocksPositionOrder);
+    }
+  }, []);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
+  const handleDragEnd = useCallback((event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setBlocksOrder((prevItems) => {
+        const oldIndex = prevItems.indexOf(active.id as string);
+        const newIndex = prevItems.indexOf(over?.id as string);
+        const newOrder = arrayMove(prevItems, oldIndex, newIndex);
+
+        localStorage.setItem(localStorageName, JSON.stringify(newOrder));
+
+        return newOrder;
+      });
+    }
+  }, []);
 
   return (
     <div className="p-2 md:p-4 max-w-[1100px] mx-auto space-y-3 ">
       <SubHeader />
+
       <MainSection />
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mt-4">
-        <DndContext>
-          <Draggable>
-            <Devices />
-            <Pages />
-            <Referrers />
-            <Countries />
-            <Events />
-            <Weekdays />
-          </Draggable>
-        </DndContext>
-      </div>
+
+      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={blocksOrder}>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mt-4">
+            {blocksOrder.map((id) => (
+              <SortableItem key={id} id={id} Component={blocksMap[id]} />
+            ))}
+          </div>
+        </SortableContext>
+      </DndContext>
     </div>
   );
 }
