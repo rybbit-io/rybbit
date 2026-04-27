2f2b65a907ee470244dba050c750fbe9c14e4c1b
LuRy <lury@lury.cz>
2026-04-27 11:24:52 +0200

    fix: range bucket off-by-one (inclusive end date)

--- client/src/components/BucketSelection.tsx
+++ client/src/components/BucketSelection.tsx
@@ -116,7 +116,7 @@ export function BucketSelection() {
     }
 
     if (time.mode === "range") {
-      const timeRangeLength = DateTime.fromISO(time.endDate).diff(DateTime.fromISO(time.startDate), "days").days;
+      const timeRangeLength = DateTime.fromISO(time.endDate).diff(DateTime.fromISO(time.startDate), "days").days + 1;
 
       return (
         <SelectContent>
