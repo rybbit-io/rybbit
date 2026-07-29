cosformula/pocketlytics

Add ads analytics endpoints and update sidebar navigation
- Introduced new API endpoint for fetching ad clicks breakdown in the server.
- Updated analytics index to export the new ad clicks breakdown function.
- Added 'Ads' section to the sidebar for navigation, enhancing user access to ad-related analytics.

server/src/index.ts
@@ -15,6 +15,7 @@ import {
   createGoal,
   deleteFunnel,
   deleteGoal,
+  getAdClicksBreakdown,
   generatePdfReport,
   getErrorBucketed,
   getErrorEvents,
@@ -268,6 +269,7 @@ async function analyticsRoutes(fastify: FastifyInstance) {
   fastify.get("/sites/:siteId/events/names", publicSite, getEventNames);
   fastify.get("/sites/:siteId/events/properties", publicSite, getEventProperties);
   fastify.get("/sites/:siteId/events/outbound", publicSite, getOutboundLinks);
+  fastify.get("/sites/:siteId/ad-clicks/breakdown", publicSite, getAdClicksBreakdown);
   fastify.get("/org-event-count/:organizationId", orgMember, getOrgEventCount);
   fastify.get("/sites/:siteId/performance/overview", publicSite, getPerformanceOverview);
   fastify.get("/sites/:siteId/performance/time-series", publicSite, getPerformanceTimeSeries);

rybbit-io/rybbit

server/src/index.ts
@@ -22,6 +22,7 @@ import {
   deleteFunnel,
   deleteGoal,
   deleteUser,
+  getAdClicksBreakdown,
   generatePdfReport,
   getDashboard,
   getDashboards,
@@ -406,6 +407,7 @@ async function analyticsRoutes(fastify: FastifyInstance) {
   fastify.get("/sites/:siteId/events/autocapture", publicEventsRead, getAutocaptureEvents);
   fastify.get("/sites/:siteId/events/autocapture-values", publicEventsRead, getAutocaptureValues);
   fastify.get("/sites/:siteId/events/outbound", publicEventsRead, getOutboundLinks);
+  fastify.get("/sites/:siteId/ad-clicks/breakdown", publicEventsRead, getAdClicksBreakdown);
   fastify.get("/org-event-count/:organizationId", orgAnalyticsRead, getOrgEventCount);
   fastify.post("/organizations/:organizationId/analytics/query", orgSqlRead, runCustomQuery);
   fastify.post("/organizations/:organizationId/analytics/query/generate", orgSqlRead, generateCustomQuery);