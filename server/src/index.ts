cosformula/pocketlytics

Refactor ads analytics implementation for improved clarity and functionality
- Renamed ad clicks-related types and functions to 'Ads' for consistency.
- Updated API endpoints to fetch ads breakdown instead of ad clicks.
- Removed deprecated ad clicks components and hooks, streamlining the codebase.
- Enhanced AdsPage to display ad impressions alongside clicks, improving analytics visibility.
- Updated tracking script to support ad impression events, expanding event tracking capabilities.

server/src/index.ts
@@ -15,7 +15,7 @@ import {
   createGoal,
   deleteFunnel,
   deleteGoal,
-  getAdClicksBreakdown,
+  getAdsBreakdown,
   generatePdfReport,
   getErrorBucketed,
   getErrorEvents,
@@ -269,7 +269,7 @@ async function analyticsRoutes(fastify: FastifyInstance) {
   fastify.get("/sites/:siteId/events/names", publicSite, getEventNames);
   fastify.get("/sites/:siteId/events/properties", publicSite, getEventProperties);
   fastify.get("/sites/:siteId/events/outbound", publicSite, getOutboundLinks);
-  fastify.get("/sites/:siteId/ad-clicks/breakdown", publicSite, getAdClicksBreakdown);
+  fastify.get("/sites/:siteId/ads/breakdown", publicSite, getAdsBreakdown);
   fastify.get("/org-event-count/:organizationId", orgMember, getOrgEventCount);
   fastify.get("/sites/:siteId/performance/overview", publicSite, getPerformanceOverview);
   fastify.get("/sites/:siteId/performance/time-series", publicSite, getPerformanceTimeSeries);

rybbit-io/rybbit

server/src/index.ts
@@ -22,7 +22,7 @@ import {
   deleteFunnel,
   deleteGoal,
   deleteUser,
-  getAdClicksBreakdown,
+  getAdsBreakdown,
   generatePdfReport,
   getDashboard,
   getDashboards,
@@ -407,7 +407,7 @@ async function analyticsRoutes(fastify: FastifyInstance) {
   fastify.get("/sites/:siteId/events/autocapture", publicEventsRead, getAutocaptureEvents);
   fastify.get("/sites/:siteId/events/autocapture-values", publicEventsRead, getAutocaptureValues);
   fastify.get("/sites/:siteId/events/outbound", publicEventsRead, getOutboundLinks);
-  fastify.get("/sites/:siteId/ad-clicks/breakdown", publicEventsRead, getAdClicksBreakdown);
+  fastify.get("/sites/:siteId/ads/breakdown", publicEventsRead, getAdsBreakdown);
   fastify.get("/org-event-count/:organizationId", orgAnalyticsRead, getOrgEventCount);
   fastify.post("/organizations/:organizationId/analytics/query", orgSqlRead, runCustomQuery);
   fastify.post("/organizations/:organizationId/analytics/query/generate", orgSqlRead, generateCustomQuery);